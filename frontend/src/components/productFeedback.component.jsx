e20ec7e98eb62f83c8e5188d3d78e8d414d48c7c
Steven Woodward <steveanthony999@gmail.com>
2022-04-06 23:31:30 -0700

    styled feedback component and added upvote styling functionality

--- frontend/src/components/productFeedback.component.jsx
+++ frontend/src/components/productFeedback.component.jsx
@@ -2,8 +2,19 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
+import IconArrowUp from '../assets/shared/icon-arrow-up.svg';
+import IconComments from '../assets/shared/icon-comments.svg';
+
 const ProductFeedback = ({ feedback }) => {
   const [commentsLength, setCommentsLength] = useState([]);
+  const [isHover, setIsHover] = useState(false);
+  const [isUpvote, setIsUpvote] = useState(false);
+
+  const handleUpvoteClick = (e) => {
+    e.preventDefault();
+    setIsUpvote((prevState) => !prevState);
+    // Dispatch upvote
+  };
 
   useEffect(() => {
     _.find(
@@ -15,18 +26,40 @@ const ProductFeedback = ({ feedback }) => {
   }, [feedback]);
 
   return (
-    <Link to={`/feedback/${feedback.id}`} state={{ feedback }}>
-      <div className='ProductFeedback'>
-        <div className='container'>
-          <div className='left'>
-            <p>{feedback.upvotes}</p>
+    <Link
+      to={`/feedback/${feedback.id}`}
+      state={{ feedback }}
+      className='ProductFeedback border'
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}>
+      <div className='container border'>
+        <div className='left'>
+          <div
+            className='btn'
+            style={{ background: isUpvote && '#4661e6' }}
+            onClick={handleUpvoteClick}>
+            <img
+              src={IconArrowUp}
+              alt='up'
+              style={{ filter: isUpvote && 'brightness(1000%)' }}
+            />
+            <p
+              className='h4 text-darker-blue'
+              style={{ color: isUpvote && '#fff' }}>
+              {feedback.upvotes}
+            </p>
           </div>
-          <div className='middle'>
-            <h3 className='h3'>{feedback.title}</h3>
-            <p className='body-1'>{feedback.description}</p>
-            <div className='btn text-blue body-3'>{feedback.category}</div>
-          </div>
-          <div className='right'>{commentsLength}</div>
+        </div>
+        <div className='middle'>
+          <h3 className={`h3 ${isHover ? 'text-blue' : 'text-darker-blue'}`}>
+            {feedback.title}
+          </h3>
+          <p className='body-1 text-grey-blue'>{feedback.description}</p>
+          <div className='btn text-blue body-3'>{feedback.category}</div>
+        </div>
+        <div className='right'>
+          <img src={IconComments} alt='bubble' />
+          <p className='text-darker-blue h4'>{commentsLength}</p>
         </div>
       </div>
     </Link>
