3fb19fcadd929d1a1ae5ad85952cadb9ee1023f9
Steven Woodward <steveanthony999@gmail.com>
2022-04-06 21:32:47 -0700

    styled button/link on topbar

--- frontend/src/components/topBar.component.jsx
+++ frontend/src/components/topBar.component.jsx
@@ -2,17 +2,22 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Select from './select.component';
 
+import SuggestionsIcon from '../assets/suggestions/icon-suggestions.svg';
+
 const TopBar = ({ passSortOrder }) => {
   const { feedback } = useSelector((state) => state.feedback);
 
   return (
-    <div className='TopBar'>
+    <div className='TopBar border'>
       <div className='left'>
+        <img src={SuggestionsIcon} alt='bulb' />
         <h3 className='h3 text-white'>{feedback.length} Suggestions</h3>
         <Select passSortOrder={passSortOrder} />
       </div>
       <div className='right'>
-        <Link to='/new-feedback'>Add Feedback</Link>
+        <Link to='/new-feedback' className='button border h4 text-very-light'>
+          + Add Feedback
+        </Link>
       </div>
     </div>
   );
