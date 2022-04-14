ac50227b2f558c223542eb07d3f03ba95fd51787
Steven Woodward <steveanthony999@gmail.com>
2022-04-14 10:01:18 -0700

    setting up replies

--- frontend/src/components/comments.component.jsx
+++ frontend/src/components/comments.component.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Comments = ({ commentProps }) => {
-  useEffect(() => {
-    console.log(`${__dirname}${commentProps.user.image}`);
-  }, [commentProps]);
+  const [isReply, setIsReply] = useState(false);
 
   return (
     <div className='Comments'>
       <div className='top'>
-        <img src={commentProps.user.image} alt='usr' />
+        <img src={commentProps.user.image} alt='usr' crossOrigin='anonymous' />
+        <div className='user-info'>
+          <h4 className='h4 text-darker-blue'>{commentProps.user.name}</h4>
+          <p className='body3 text-grey-blue'>@{commentProps.user.username}</p>
+        </div>
+        <button onClick={() => setIsReply((prevState) => !prevState)}>
+          Reply
+        </button>
       </div>
       <p>{commentProps.content}</p>
       <hr />
@@ -16,6 +21,7 @@ const Comments = ({ commentProps }) => {
         commentProps.replies.map((reply, index) => (
           <p key={index}>{reply.content}</p>
         ))}
+      {isReply && <div className='reply-container'>Hello reply</div>}
     </div>
   );
 };
