0edd672eade35f38ecfa8d16dd813915bf927463
Steven Woodward <steveanthony999@gmail.com>
2022-04-14 20:29:29 -0700

    styling replies

--- frontend/src/components/replies.component.jsx
+++ frontend/src/components/replies.component.jsx
@@ -1,23 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import Replies from './replies.component';
 import Reply from './reply.component';
 
-const Comments = ({ commentProps }) => {
+const Replies = ({ reply }) => {
   const [isReply, setIsReply] = useState(false);
 
+  useEffect(() => {
+    console.log(reply);
+  }, [reply]);
+
   return (
-    <div className='Comments'>
+    <div className='Replies'>
       <div className='top'>
         <img
-          src={commentProps.user.image}
+          src={reply.user.image}
           alt='usr'
           className='user-image'
           crossOrigin='anonymous'
         />
         <div className='user-info'>
-          <h4 className='h4 text-darker-blue'>{commentProps.user.name}</h4>
-          <p className='body3 text-grey-blue'>@{commentProps.user.username}</p>
+          <h4 className='h4 text-darker-blue'>{reply.user.name}</h4>
+          <p className='body3 text-grey-blue'>@{reply.user.username}</p>
         </div>
         <button
           className='text-blue body-3'
@@ -26,19 +29,20 @@ const Comments = ({ commentProps }) => {
         </button>
       </div>
       <div className='middle'>
-        <p className='body-2 text-grey-blue'>{commentProps.content}</p>
+        <p className='body-2 text-grey-blue'>
+          <span className='h4 text-purple'>@{reply.replyingTo}</span>
+          &nbsp;&nbsp;
+          {reply.content}
+        </p>
+      </div>
+      <div className='bottom'>
         {isReply && (
           <div className='reply-container'>
             <Reply />
           </div>
         )}
-        {!commentProps.replies && <hr />}
       </div>
-      {commentProps.replies &&
-        commentProps.replies.map((reply, index) => (
-          <Replies key={index} reply={reply} />
-        ))}
     </div>
   );
 };
-export default Comments;
+export default Replies;
---
+++ frontend/src/components/reply.component.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const Reply = () => {
+  const [text, setText] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(text);
+    setText('');
+  };
+
+  return (
+    <div className='Reply'>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className='border body-2 text-darker-blue'
+          name='reply'
+          id='reply'
+          value={text}
+          onChange={(e) => setText(e.target.value)}></textarea>
+        <button className='text-very-light h4 border'>Post Reply</button>
+      </form>
+    </div>
+  );
+};
+export default Reply;
