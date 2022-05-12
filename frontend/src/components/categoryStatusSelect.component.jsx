7c597b1f482612dd2208c00790b7ad10f7878cde
Steven Woodward <steveanthony999@gmail.com>
2022-05-12 09:00:21 -0700

    refactored

---
+++ frontend/src/components/categoryStatusSelect.component.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+
+import IconArrowDown from '../assets/shared/icon-arrow-down.svg';
+import IconArrowUp from '../assets/shared/icon-arrow-up.svg';
+import IconCheck from '../assets/shared/icon-check.svg';
+
+import '../styles/components/categoryStatusSelect.css';
+
+const CategoryStatusSelect = ({
+  title,
+  subtitle,
+  options,
+  initialCategoryStatus,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [isChecked, setIsChecked] = useState(initialCategoryStatus);
+
+  const toggling = () => {
+    setIsOpen((prevValue) => !prevValue);
+  };
+
+  const onOptionClicked = (value) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    setIsChecked(value);
+    document.removeEventListener('click', () => console.log('done'));
+  };
+
+  const handleBodyClick = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isOpen === true) {
+      document.addEventListener('click', handleBodyClick);
+    }
+    return () => document.removeEventListener('click', handleBodyClick);
+  }, [isOpen]);
+
+  return (
+    <div className='category-status-select-container'>
+      <h4 className='h4 text-darker-blue'>{title}</h4>
+      <p className='body-2 text-grey-blue'>{subtitle}</p>
+      <div
+        className={`category-status-select-field border ${
+          isOpen && 'category-status-select-active'
+        }`}
+        onClick={toggling}
+        style={{ opacity: isOpen ? 0.8 : 1 }}>
+        <span className='body-2 text-darker-blue'>{selectedOption}</span>
+        <img
+          className='category-status-select-field-img'
+          src={isOpen ? IconArrowUp : IconArrowDown}
+          alt='arrow'
+        />
+      </div>
+      {isOpen && (
+        <div className='category-status-select-list-container'>
+          <ul className='category-status-select-list border'>
+            {options.map((option, index) => (
+              <li
+                className='category-status-select-list-item body-1'
+                onClick={onOptionClicked(option)}
+                key={index}>
+                {option}
+                {isChecked === option && <img src={IconCheck} alt='check' />}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+export default CategoryStatusSelect;
--- frontend/src/pages/new-feedback.page.jsx
+++ frontend/src/pages/new-feedback.page.jsx
@@ -1,50 +1,18 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import IconArrowDown from '../assets/shared/icon-arrow-down.svg';
-import IconArrowUp from '../assets/shared/icon-arrow-up.svg';
 import IconNewFeedback from '../assets/shared/icon-new-feedback.svg';
-import IconCheck from '../assets/shared/icon-check.svg';
 
-import '../styles/pages/newFeedbackPage.css';
 import GoBack from '../components/goBack.component';
-
-// const options = ['Feature', 'UI', 'UX', 'Enhancement', 'Bug'];
+import CategoryStatusSelect from '../components/categoryStatusSelect.component';
 
 import options from '../options.json';
 
+import '../styles/pages/newFeedbackPage.css';
+
 const NewFeedback = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(
-    options['feedback-categories'][0]
-  );
-  const [isChecked, setIsChecked] = useState('Feature');
-
-  const toggling = () => {
-    setIsOpen((prevValue) => !prevValue);
-  };
-
-  const onOptionClicked = (value) => () => {
-    setSelectedOption(value);
-    setIsOpen(false);
-    setIsChecked(value);
-    document.removeEventListener('click', () => console.log('done'));
-  };
-
-  const handleBodyClick = () => {
-    setIsOpen(false);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
   };
-
-  useEffect(() => {
-    if (isOpen === true) {
-      document.addEventListener('click', handleBodyClick);
-    }
-    return () => document.removeEventListener('click', handleBodyClick);
-  }, [isOpen]);
 
   return (
     <div className='NewFeedbackPage'>
@@ -68,46 +36,12 @@ const NewFeedback = () => {
               type='text'
               className='input-field border body-2 text-darker-blue'
             />
-            {/* SELECT */}
-            <div className='new-feedback-select-container'>
-              <h4 className='h4 text-darker-blue'>Category</h4>
-              <p className='body-2 text-grey-blue'>
-                Choose a category for your feedback
-              </p>
-              <div
-                className={`select-field border ${
-                  isOpen && 'new-feedback-select-active'
-                }`}
-                onClick={toggling}
-                style={{ opacity: isOpen ? 0.8 : 1 }}>
-                <span className='body-2 text-darker-blue'>
-                  {selectedOption}
-                </span>
-                <img
-                  className='select-field-img'
-                  src={isOpen ? IconArrowUp : IconArrowDown}
-                  alt='arrow'
-                />
-              </div>
-              {isOpen && (
-                <div className='select-list-container'>
-                  <ul className='select-list border'>
-                    {options['feedback-categories'].map((option, index) => (
-                      <li
-                        className='select-list-item body-1'
-                        onClick={onOptionClicked(option)}
-                        key={index}>
-                        {option}
-                        {isChecked === option && (
-                          <img src={IconCheck} alt='check' />
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-            {/* END SELECT */}
+            <CategoryStatusSelect
+              title='Category'
+              subtitle='Choose a category for your feedback'
+              options={options['feedback-categories']}
+              initialCategoryStatus='Feature'
+            />
             <div className='new-feedback-detail'>
               <h4 className='h4 text-darker-blue'>Feedback Detail</h4>
               <p className='body-2 text-grey-blue'>
