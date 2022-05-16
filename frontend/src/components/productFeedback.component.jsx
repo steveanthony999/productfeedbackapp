cb551245637eb44b317a8b418245f84932d0d6b1
Steven Woodward <steveanthony999@gmail.com>
2022-05-15 19:49:11 -0700

    made responsive

--- frontend/src/components/productFeedback.component.jsx
+++ frontend/src/components/productFeedback.component.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
+import { useMediaQuery } from 'react-responsive';
 
 import IconArrowUp from '../assets/shared/icon-arrow-up.svg';
 import IconComments from '../assets/shared/icon-comments.svg';
@@ -9,6 +10,8 @@ import axios from 'axios';
 import '../styles/components/productFeedback.css';
 
 const ProductFeedback = ({ feedback }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 738px)' });
+
   const [commentsLength, setCommentsLength] = useState([]);
   const [repliesLength, setRepliesLength] = useState([]);
   const [isHover, setIsHover] = useState(false);
@@ -59,6 +62,52 @@ const ProductFeedback = ({ feedback }) => {
         )
     );
   }, [feedback]);
+
+  if (isMobile) {
+    return (
+      <Link
+        to={`/feedback/${feedback.id}`}
+        state={{ feedback }}
+        className='ProductFeedback-mobile border'
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}>
+        <div className='ProductFeedback-container-mobile border'>
+          <div className='ProductFeedback-middle-mobile'>
+            <h3 className={`h3 ${isHover ? 'text-blue' : 'text-darker-blue'}`}>
+              {feedback.title}
+            </h3>
+            <p className='body-1 text-grey-blue'>{feedback.description}</p>
+            <div className='btn text-blue body-3'>{feedback.category}</div>
+            <div className='ProductFeedback-bottom-mobile'>
+              <div className='ProductFeedback-bottom-container-mobile'>
+                <div
+                  className='upvote-btn'
+                  style={{ background: isUpvote && '#4661e6' }}
+                  onClick={handleUpvoteClick}>
+                  <img
+                    src={IconArrowUp}
+                    alt='up'
+                    style={{ filter: isUpvote && 'brightness(1000%)' }}
+                  />
+                  <p
+                    className='h4 text-darker-blue'
+                    style={{ color: isUpvote && '#fff' }}>
+                    {feedbackUpvotes}
+                  </p>
+                </div>
+              </div>
+              <div className='ProductFeedback-right-mobile'>
+                <img src={IconComments} alt='bubble' />
+                <p className='text-darker-blue h4'>
+                  {commentsLength + repliesLength}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 
   return (
     <Link
--- frontend/src/components/topBarHome.component.jsx
+++ frontend/src/components/topBarHome.component.jsx
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+
 import TopbarSelect from './topbarSelect.component';
 
 import SuggestionsIcon from '../assets/suggestions/icon-suggestions.svg';
 
 import options from '../options.json';
 
-import '../styles/helpers/globals.css';
 import '../styles/components/topBarHome.css';
 
 const TopBarHome = ({ passSortOrder }) => {
   const { feedback } = useSelector((state) => state.feedback);
+  const isMobile = useMediaQuery({ query: '(max-width: 738px)' });
 
   return (
     <div className='TopBarHome border'>
       <div className='TopBarHome-left'>
-        <img src={SuggestionsIcon} alt='bulb' />
-        <h3 className='h3 text-white'>{feedback.length} Suggestions</h3>
+        {!isMobile && (
+          <>
+            <img src={SuggestionsIcon} alt='bulb' />
+            <h3 className='h3 text-white'>{feedback.length} Suggestions</h3>
+          </>
+        )}
         <TopbarSelect
           options={options['home-top-bar-options']}
           passSortOrder={passSortOrder}
