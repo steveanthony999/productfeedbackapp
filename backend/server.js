ddb38f5784f92222f9bb72629ac56609a6c2537c
Steven Woodward <steveanthony999@gmail.com>
2022-05-28 19:00:10 -0700

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
 // Connect to database
