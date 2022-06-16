806b7f5264508f9666ca12c0817201113e879aa5
Steven Woodward <steveanthony999@gmail.com>
2022-06-15 17:08:09 -0700

    added userbox to sidebar

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,8 @@
+import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 
 import CategoryBox from './categoryBox.component';
+import UserBox from '../components/userBox.component';
 
 import '../styles/components/sidebar.css';
 import RoadmapBox from './roadmapBox.component';
@@ -8,11 +10,33 @@ import RoadmapBox from './roadmapBox.component';
 const Sidebar = ({ isMenuOpen, feedback }) => {
   const isMobile = useMediaQuery({ query: '(max-width: 738px)' });
 
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const { currentUser } = useSelector((state) => state.auth);
+
   return (
     <div className='Sidebar' style={{ right: isMenuOpen ? 0 : '-100vw' }}>
       <div className='Sidebar-container'>
         {isMobile && <CategoryBox />}
         {isMobile && <RoadmapBox feedback={feedback} />}
+        {isMobile && (
+          <UserBox
+            user={user}
+            feedbackLength={
+              currentUser &&
+              currentUser.feedbackId &&
+              currentUser.feedbackId.length
+            }
+            upvotesLength={
+              currentUser && currentUser.upvoteId && currentUser.upvoteId.length
+            }
+            commentsLength={
+              currentUser &&
+              currentUser.commentId &&
+              currentUser.commentId.length
+            }
+          />
+        )}
       </div>
     </div>
   );
