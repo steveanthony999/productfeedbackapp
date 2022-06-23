7b3a2e079e10d3e3067a36c045937324cee8925b
Steven Woodward <steveanthony999@gmail.com>
2022-06-23 15:38:06 -0700

    set up for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -16,6 +17,18 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/feedback', require('./routes/feedbackRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => res.status(200).json({ message: 'Sup' }));
+}
 
 app.use(errorHandler);
 
